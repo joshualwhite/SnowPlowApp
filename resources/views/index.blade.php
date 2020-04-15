d36a2d81edb859e92326e74e815dd4b693388ea9
Josh White <mrjoshualwhite@gmail.com>
2020-04-15 16:19:45 -0500

    Small changes, hide /register from 'Guest' level

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="jumbotron text-center bg-light">
-        <h1>Welcome To Snow Plow App!</h1>
-        <p>This app was created by two UND students for Garden Hut, LLC.</p>
+        <h1>Garden Hut Snow Plow Routes</h1>
+        <p>In order to access the app you must be an employee of the Garden Hut.</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a></p>
     </div>
 @endsection
